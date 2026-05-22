471b770f610680f61ee771e16fa64a5ca47a47de
Pisuth <pisuth.dae@gmail.com>
2026-05-22 19:25:17 +0700

    Simplify CLI - single command to start API server

--- src/cli.js
+++ src/cli.js
@@ -1,39 +1,12 @@
 #!/usr/bin/env node
 
 import chalk from 'chalk';
-import { Command } from 'commander';
 import { startServer } from './server.js';
 
-const program = new Command();
+async function main() {
+  console.log(chalk.blue('🚀 Starting Predict Station API server...'));
+  console.log(chalk.cyan('  → Listening on http://localhost:3001'));
+  await startServer(3001);
+}
 
-program
-  .name('predict-station')
-  .description('CLI for Predict Station')
-  .version('0.1.0');
-
-program
-  .command('init')
-  .description('Initialize a new Predict Station project')
-  .action(() => {
-    console.log(chalk.blue('🎯 Welcome to Predict Station!'));
-    console.log(chalk.green('Project initialized successfully.'));
-    console.log(chalk.gray('Run npm run dev to start the API server.'));
-  });
-
-program
-  .command('dev')
-  .description('Start the HTTP API server on port 3001')
-  .action(async () => {
-    console.log(chalk.blue('🚀 Starting Predict Station API server...'));
-    await startServer(3001);
-  });
-
-program
-  .command('server')
-  .description('Start the HTTP API server')
-  .action(async () => {
-    console.log(chalk.blue('🔌 Starting HTTP API server...'));
-    await startServer(3001);
-  });
-
-program.parse(process.argv);
+main();
